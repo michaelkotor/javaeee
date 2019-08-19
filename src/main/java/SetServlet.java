1dde25d31923dbe12ebc39ba79c7f36c31b0276a
Michael Kotor <milan3020@gmail.com>
2019-08-19 10:16:36 +0300

    init

---
+++ src/main/java/SetServlet.java
@@ -0,0 +1,11 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/set")
+public class SetServlet extends HttpServlet {
+    protected void doGet(HttpServletResponse response, HttpServletRequest request) {
+
+    }
+}
